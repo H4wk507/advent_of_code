9579d237d52f86b8e7c11acfc97724f6ffb7f148
H4wk <quasand89@gmail.com>
2022-12-14 03:34:18 +0100

    improve from 4.3s to 0.04s

--- 2022/day12/main.py
+++ 2022/day12/main.py
@@ -1,75 +1,55 @@
 import math
 
 
-def handle_elevation(grid, cx, cy, nx, ny):
-    if (cx, cy) == (0, 0):
-        return True
-
-    if grid[cy][cx] == "S":
-        return ord("a") + 1 >= ord(grid[ny][nx])
-
-    if grid[ny][nx] == "E":
-        return ord(grid[cy][cx]) + 1 >= ord("z")
-    return ord(grid[cy][cx]) + 1 >= ord(grid[ny][nx])
+def handle_elevation(
+    grid: list[list[str]], cx: int, cy: int, nx: int, ny: int
+) -> bool:
+    if grid[ny][nx] == "S":
+        return ord(grid[cy][cx]) - 1 <= ord("a")
+    else:
+        return ord(grid[cy][cx]) - 1 <= ord(grid[ny][nx])
 
 
-def get_start_cords(grid, R, C):
-    for y in range(R):
-        for x in range(C):
-            if grid[y][x] == "S":
-                return (y, x)
-    return (-1, -1)
+def get_start_cords(grid: list[list[str]], R: int, C: int) -> tuple[int, int]:
+    return [(y, x) for x in range(C) for y in range(R) if grid[y][x] == "E"][0]
 
 
-def get_a_cords(grid, R, C, lst):
-    for y in range(R):
-        for x in range(C):
-            if grid[y][x] == "a":
-                lst.append((y, x))
+def solve(grid: list[list[str]], R: int, C: int) -> None:
+    E = get_start_cords(grid, R, C)
+    part1 = part2 = math.inf
+    q = []
+    q.append(E)
+    # min dist to [i][j]
+    dist = [[math.inf for _ in range(C)] for _ in range(R)]
+    dist[E[0]][E[1]] = 0
+    grid[E[0]][E[1]] = "z"
+    while q:
+        v = q.pop(0)
+        vy, vx = v[0], v[1]
+        if grid[vy][vx] == "S":
+            part1 = min(part1, dist[vy][vx])
+        if grid[vy][vx] in ("a", "S"):
+            part2 = min(part2, dist[vy][vx])
+        for dx, dy in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+            ny = vy + dy
+            nx = vx + dx
+            if nx not in range(C) or ny not in range(R):
+                continue
+            curr_dist = dist[vy][vx] + 1
+            if curr_dist < dist[ny][nx] and handle_elevation(
+                grid, vx, vy, nx, ny
+            ):
+                dist[ny][nx] = curr_dist
+                q.append((ny, nx))
 
-
-def solve(rows: list[str], part):
-    R = len(rows)
-    C = len(rows[0])
-    grid = [[c for c in row] for row in rows]
-
-    S = get_start_cords(grid, R, C)
-    start_indices = [S]
-    if part == 2:
-        get_a_cords(grid, R, C, start_indices)
-    global_min = math.inf
-    for ay, ax in start_indices:
-        q = []
-        q.append((ay, ax))
-        # min dist to [i][j]
-        dist = [[math.inf for _ in range(C)] for _ in range(R)]
-        dist[ay][ax] = 0
-        while q:
-            v = q.pop(0)
-            vy, vx = v[0], v[1]
-            if grid[vy][vx] == "E":
-                global_min = min(global_min, dist[vy][vx])
-                break
-            for dx, dy in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
-                ny = vy + dy
-                nx = vx + dx
-
-                if nx not in range(C) or ny not in range(R):
-                    continue
-
-                curr_dist = dist[vy][vx] + 1
-                if dist[ny][nx] > curr_dist and handle_elevation(
-                    grid, vx, vy, nx, ny
-                ):
-                    dist[ny][nx] = curr_dist
-                    q.append((ny, nx))
-    return global_min
+    print(f"Part #1: {part1}")
+    print(f"Part #2: {part2}")
 
 
 if __name__ == "__main__":
-    file = "input.txt"
-    with open(file) as f:
+    with open("input.txt") as f:
         rows = f.read().strip("\n").split("\n")
-
-    print(f"Part #1: {solve(rows, 1)}")
-    print(f"Part #2: {solve(rows, 2)}")
+    R = len(rows)
+    C = len(rows[0])
+    grid = [[c for c in row] for row in rows]
+    solve(grid, R, C)
