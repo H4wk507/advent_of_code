4e31fa6378e38b7c847d8ee3e33cbb98c7eea49f
H4wk <quasand89@gmail.com>
2024-12-14 03:15:50 +0100

    d5: zig bit shifts opt 1.5ms -> 0.477ms

--- 2024/day5/src/main.zig
+++ 2024/day5/src/main.zig
@@ -1,95 +1,84 @@
 const std = @import("std");
 
-inline fn array_contains(comptime T: type, haystack: []T, needle: T) bool {
-    return for (haystack) |element| {
-        if (element == needle) break true;
-    } else false;
-}
-
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
     const file_content = try std.fs.cwd().readFileAlloc(allocator, "dat", 1024 * 1024);
-    var lines_it = std.mem.splitSequence(u8, file_content, "\n");
 
-    var map = std.AutoHashMap(u64, std.ArrayList(u64)).init(allocator);
-    defer {
-        var it = map.valueIterator();
-        while (it.next()) |list| {
-            list.deinit();
-        }
-        map.deinit();
-    }
-    while (lines_it.next()) |line| {
+    var sections = std.mem.splitSequence(u8, file_content, "\n\n");
+    const rules_section = sections.next() orelse return error.InvalidFormat;
+    const updates_section = sections.next() orelse return error.InvalidFormat;
+
+    var map = std.AutoHashMap(u64, u128).init(allocator);
+    defer map.deinit();
+
+    var rules_lines = std.mem.splitSequence(u8, rules_section, "\n");
+    while (rules_lines.next()) |line| {
         if (line.len == 0) break;
 
         var pages = std.mem.splitSequence(u8, line, "|");
-        const before = try std.fmt.parseInt(u64, pages.next() orelse return error.InvalidFormat, 10);
+        const before = try std.fmt.parseInt(u7, pages.next() orelse return error.InvalidFormat, 10);
         const y = try std.fmt.parseInt(u64, pages.next() orelse return error.InvalidFormat, 10);
 
-        var entry = try map.getOrPut(y);
+        const entry = try map.getOrPut(y);
         if (!entry.found_existing) {
-            entry.value_ptr.* = std.ArrayList(u64).init(allocator);
+            entry.value_ptr.* = 0;
         }
-        try entry.value_ptr.append(before);
+        entry.value_ptr.* |= (@as(u128, 1) << before);
     }
 
     var sum1: u64 = 0;
     var sum2: u64 = 0;
-    while (lines_it.next()) |line| {
+    var updates = std.mem.splitSequence(u8, updates_section, "\n");
+    while (updates.next()) |line| {
         if (line.len == 0) break;
 
-        var nums = std.ArrayList(u64).init(allocator);
-        defer nums.deinit();
-        var nums_it = std.mem.splitSequence(u8, line, ",");
+        var nums_buf: [128]u64 = undefined;
+        var nums_count: usize = 0;
 
+        var nums_it = std.mem.splitSequence(u8, line, ",");
         while (nums_it.next()) |n_string| {
             if (n_string.len == 0) break;
-            try nums.append(try std.fmt.parseInt(u64, n_string, 10));
+            nums_buf[nums_count] = try std.fmt.parseInt(u64, n_string, 10);
+            nums_count += 1;
         }
 
-        if (nums.items.len == 0) continue;
+        if (nums_count == 0) continue;
+
+        var current_set: u128 = 0;
+        for (nums_buf[0..nums_count]) |num| {
+            current_set |= (@as(u128, 1) << @as(u7, @intCast(num)));
+        }
 
         var ok = true;
-        var consumed = std.ArrayList(u64).init(allocator);
-        defer consumed.deinit();
+        var consumed: u128 = 0;
 
-        for (nums.items) |num| {
-            if (map.get(num)) |befores| {
-                for (befores.items) |before| {
-                    if (!array_contains(u64, nums.items, before)) continue;
-                    if (!array_contains(u64, consumed.items, before)) {
-                        ok = false;
-                        break;
-                    }
+        for (nums_buf[0..nums_count]) |num| {
+            if (map.get(num)) |before_set| {
+                if ((before_set & current_set) != 0 and (before_set & current_set) != (before_set & consumed)) {
+                    ok = false;
+                    break;
                 }
-                if (!ok) break;
             }
-            try consumed.append(num);
+            consumed |= @as(u128, 1) << @as(u7, @intCast(num));
         }
 
-        const middle_idx = nums.items.len / 2;
+        const middle_idx = nums_count / 2;
         if (ok) {
-            sum1 += nums.items[middle_idx];
+            sum1 += nums_buf[middle_idx];
         } else {
-            var valid_arr = try allocator.alloc(u64, nums.items.len);
-            defer allocator.free(valid_arr);
-            @memset(valid_arr, 0);
+            var valid: [128]u64 = undefined;
 
-            for (nums.items) |num| {
-                var idx: usize = 0;
-                if (map.get(num)) |befores| {
-                    for (befores.items) |before| {
-                        if (array_contains(u64, nums.items, before)) {
-                            idx += 1;
-                        }
-                    }
+            for (nums_buf[0..nums_count]) |num| {
+                var pos: usize = 0;
+                if (map.get(num)) |before_set| {
+                    pos = @popCount(before_set & current_set);
                 }
-                valid_arr[idx] = num;
+                valid[pos] = num;
             }
-            sum2 += valid_arr[middle_idx];
+            sum2 += valid[middle_idx];
         }
     }
     std.debug.print("sum1: {}\n", .{sum1});
